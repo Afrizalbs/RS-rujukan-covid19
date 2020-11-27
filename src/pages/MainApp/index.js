5bae79494687eaaece8fdbf4ac139d9990478787
Afrizal <afrizalbagas.03@gmail.com>
2020-11-27 23:31:58 +0700

    setting react router

---
+++ src/pages/MainApp/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainApp() {
+  return (
+    <div>
+      <p>Header</p>
+      <p>content</p>
+      <p>footer</p>
+    </div>
+  );
+}
+
+export default MainApp;
